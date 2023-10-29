91740fbabe00b89af7bd2fdcbc7b4eff68a2fcfd
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-10-30 00:04:40 +0100

    Usunięcie srednika

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Uczę\nsię\nprogramowania\ni\ncommitowania");;
+        System.out.print("Uczę\nsię\nprogramowania\ni\ncommitowania");
     }
 }
